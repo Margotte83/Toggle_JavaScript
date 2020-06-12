2f86238ba6844a2ed062c4b246a86f574423b282
Margotte83 <margotreize@live.fr>
2020-06-12 17:50:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button');
+const text = document.querySelector('.text');
+
+btn.addEventListener('click', () => {
+    text.classList.toggle('is-visible');
+
+});
